4355dc71224eaf1f186b6a899aec7037adcb718a
Poooowero <bartek2002@interia.pl>
2022-09-28 17:22:18 +0200

    settings added

---
+++ app/src/main/java/com/example/myapplication/myapplication/ui/settings/SettingsFragment.java
@@ -0,0 +1,41 @@
+package com.example.myapplication.myapplication.ui.settings;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.viewbinding.ViewBinding;
+
+import com.example.myapplication.myapplication.databinding.FragmentSettingsBinding;
+import com.example.myapplication.myapplication.ui.home.HomeViewModel;
+
+
+public class SettingsFragment extends Fragment {
+
+    private FragmentSettingsBinding binding;
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        SettingsViewModel settingsViewModel =
+                new ViewModelProvider(this).get(SettingsViewModel.class);
+
+        binding = FragmentSettingsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textSettings;
+        settingsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
