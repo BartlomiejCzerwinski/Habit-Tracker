a458aa06ebe27781582135f676dc9bda5adfdc41
Poooowero <bartek2002@interia.pl>
2022-09-28 19:54:00 +0200

    statistics added

---
+++ app/src/main/java/com/example/myapplication/myapplication/ui/statistics/StatisticsFragment.java
@@ -0,0 +1,39 @@
+package com.example.myapplication.myapplication.ui.statistics;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.myapplication.myapplication.databinding.FragmentStatisticsBinding;
+import com.example.myapplication.myapplication.ui.statistics.StatisticsViewModel;
+
+public class StatisticsFragment extends Fragment {
+
+    private FragmentStatisticsBinding binding;
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        StatisticsViewModel statisticsViewModel =
+                new ViewModelProvider(this).get(StatisticsViewModel.class);
+
+        binding = FragmentStatisticsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textStatistics;
+        statisticsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
